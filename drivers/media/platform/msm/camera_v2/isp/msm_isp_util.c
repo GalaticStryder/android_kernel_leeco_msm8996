b10d177873e1753ebc9fc4213b0ec493cf533d24
Ícaro Hoff <icarohoff@gmail.com>
2017-05-25 10:51:11 -0300

    camera_v2: port oneplus OEM changes to our LA.UM Kernel
    W.I.P.P.E.R.S.
    
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -185,7 +185,7 @@ uint32_t msm_isp_get_framedrop_period(
 void msm_isp_get_timestamp(struct msm_isp_timestamp *time_stamp)
 {
 	struct timespec ts;
-	get_monotonic_boottime(&ts);
+	ktime_get_ts(&ts);
 	time_stamp->buf_time.tv_sec    = ts.tv_sec;
 	time_stamp->buf_time.tv_usec   = ts.tv_nsec/1000;
 	do_gettimeofday(&(time_stamp->event_time));
