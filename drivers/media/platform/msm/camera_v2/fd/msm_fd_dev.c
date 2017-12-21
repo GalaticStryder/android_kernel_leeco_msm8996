df1ff41cb1985ab70981b19a1c3de9e044c3d74e
Ícaro Hoff <icarohoff@gmail.com>
2017-12-21 11:10:05 -0200

    Revert "camera_v2: port oneplus OEM changes to our LA.UM Kernel"
    Bringing it closer to CAF's code base for easier merging and use
    monotonic clock for timestamps.
    
    This reverts commit af72a78a3dc34eac7d6bd9c431c949757ba0130f.
    
    Change-Id: I01f373ed93cfe675ab35506e2de774203345553f
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
+++ drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
@@ -339,9 +339,10 @@ static int msm_fd_vbif_error_handler(void *handle, uint32_t error)
 	struct msm_fd_buffer *active_buf;
 	int ret;
 
-	if (handle == NULL)
+	if (NULL == handle) {
+		dev_err(fd->dev, "FD Ctx is null, Cannot recover\n");
 		return 0;
-
+	}
 	ctx = (struct fd_ctx *)handle;
 	fd = (struct msm_fd_device *)ctx->fd_device;
 
