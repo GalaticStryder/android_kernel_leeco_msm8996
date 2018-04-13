ae91b251dab6ff8e775bbf34dedb8c0dc12f0fbe
Vijay Kumar TM <vtmuni@codeaurora.org>
2018-04-13 18:48:49 +0530

    mm-camera2: isp2: Release buffer lock after use
    At start axi stream, we are acquiring the
    buffer lock and releasing after completing
    the stream configuration operations. In case
    of live snapshot,this is causing the buffer
    operations to halt and leading to sof freeze.
    
    CRs-Fixed: 2149998
    Change-Id: I7f57c8befc85c105a5690ac71e0276f42b9f1929
    Signed-off-by: Meera Gande <mgande@codeaurora.org>
    Signed-off-by: Vijay Kumar TM <vtmuni@codeaurora.org>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -811,6 +811,7 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 		break;
 	case VIDIOC_MSM_ISP_AXI_RESTART:
 		mutex_lock(&vfe_dev->core_mutex);
+		mutex_lock(&vfe_dev->buf_mgr->lock);
 		if (atomic_read(&vfe_dev->error_info.overflow_state)
 			!= HALT_ENFORCED) {
 			rc = msm_isp_stats_restart(vfe_dev);
@@ -821,6 +822,7 @@ static long msm_isp_ioctl_unlocked(struct v4l2_subdev *sd,
 			pr_err_ratelimited("%s: no AXI restart, halt enforced.\n",
 				__func__);
 		}
+		mutex_unlock(&vfe_dev->buf_mgr->lock);
 		mutex_unlock(&vfe_dev->core_mutex);
 		break;
 	case VIDIOC_MSM_ISP_INPUT_CFG:
